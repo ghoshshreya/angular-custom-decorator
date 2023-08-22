10875cd75384ebb6fdb3ce509a3b5c254093272f
ghoshshreya <shrghosh08@gmail.com>
2023-08-22 18:41:15 +0530

    1 file modified

--- src/logger-component/logger-component.component.ts
+++ src/logger-component/logger-component.component.ts
@@ -3,18 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-logger-component',
   standalone: true,
-  templateUrl: './logger-component.component.html',
+  template: '<div> Logger Component </div>',
   styleUrls: ['./logger-component.component.css'],
 })
 export class LoggerComponentComponent implements OnInit {
   constructor() {}
   @log()
   ngOnInit() {
-    this.add(10, 32);
+    console.log('Inside ngOninit');
+    console.log(this.add(10, 32));
+  }
+
+  @log()
+  randomFunction() {
+    console.log('Inside randomFunction');
   }
 
   @log()
   private add(a: number, b: number) {
+    console.log('Inside add');
     return a + b;
   }
 }
@@ -26,18 +33,26 @@ Name of the property or method being decorated
 descriptor describes the property or method being decorated. This object contains information such as the property's name, type, value, and visibility.
 ---*/
 
-export function log(): MethodDecorator {
-  return function (target: Function, key: string, descriptor: any) {
-    const originalMethod = descriptor.value;
-
+export function log() {
+  return function (target: any, key: string, descriptor: any) {
+    const originalMd = descriptor.value;
     descriptor.value = function (...args: any[]) {
-      console.log(`Entering ${key} method`);
-      const result = originalMethod.apply(this, args);
-      console.log(`Leaving ${key} method`);
-
+      console.log(`Entering ${key}`);
+      const result = originalMd.apply(this, args);
+      console.log(`Leaving ${key}`);
       return result;
     };
+    return descriptor.value;
+    // const originalMethod = descriptor.value;
 
-    return descriptor;
+    // descriptor.value = function (...args: any[]) {
+    //   console.log(`Entering ${key} method`);
+    //   const result = originalMethod.apply(this, args);
+    //   console.log(`Leaving ${key} method`);
+
+    //   return result;
+    // };
+
+    // return descriptor;
   };
 }
